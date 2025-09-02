d2f32f49f8f28a8e1961a0475de4665a8374879d
kikejfer <enferlo@gmail.com>
2025-09-02 12:26:41 +0200

    Fix database import path in test-metadata route
    - Change from '../config/database' to '../database/connection'
    - Match the import pattern used by other routes in the project

--- routes/test-metadata.js
+++ routes/test-metadata.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../config/database');
+const pool = require('../database/connection');
 
 // Test endpoint to verify metadata tables and data
 router.get('/', async (req, res) => {
